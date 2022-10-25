08051c5b0f24dba7e05068f9dfb2f6f3ffe270dc
PrakatNeupane <60876051+PrakatNeupane@users.noreply.github.com>
2022-10-25 20:09:13 +1100

    dispatch actions in component to fetch data

--- src/components/DisplayTable.jsx
+++ src/components/DisplayTable.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import {
+  setDataFromApi,
+  setDataToDisplay,
+  setIsLoading,
+} from "../state-management/appSlice";
+import { fetchDataFromApi } from "../helper/axiosHelper";
+import { useDispatch, useSelector } from "react-redux";
 
 const DisplayTable = () => {
-  return <div>DisplayTable</div>;
-};
+  const dispatch = useDispatch();
+  const { isLoading, dataToDisplay } = useSelector((state) => state.apiData);
 
+  const handleOnLoad = () => {
+    dispatch(setIsLoading(true));
+    fetchDataFromApi().then((data) => {
+      dispatch(setDataFromApi(data));
+      dispatch(setDataToDisplay(data));
+      dispatch(setIsLoading(false));
+    });
+  };
+  return <div></div>;
+};
 export default DisplayTable;
