9bfee8942e594bfbdb2672eec2e96bf319fbf3c5
PrakatNeupane <60876051+PrakatNeupane@users.noreply.github.com>
2022-10-25 20:12:49 +1100

    react-bootstrap table import and assigning functions to onclick while also importing component to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import DisplayTable from './components/DisplayTable';
 function App() {
   return (
     <div className="App">
-
+      <DisplayTable />
     </div>
   );
 }
--- src/components/DisplayTable.jsx
+++ src/components/DisplayTable.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import { useDispatch, useSelector } from "react-redux";
 import {
+  addData,
+  deleteData,
   setDataFromApi,
   setDataToDisplay,
   setIsLoading,
 } from "../state-management/appSlice";
 import { fetchDataFromApi } from "../helper/axiosHelper";
-import { useDispatch, useSelector } from "react-redux";
+import Spin from "./Spin";
 
 const DisplayTable = () => {
   const dispatch = useDispatch();
@@ -19,6 +24,54 @@ const DisplayTable = () => {
       dispatch(setIsLoading(false));
     });
   };
-  return <div></div>;
+  const handleOnDelete = () => {
+    dispatch(deleteData());
+  };
+
+  const handleOnAdd = () => {
+    dispatch(addData());
+  };
+  return (
+    <div>
+      {isLoading && <Spin />}
+
+      <div className="buttons">
+        <Button className="btn btn-primary me-1" onClick={handleOnLoad}>
+          LOAD
+        </Button>
+        <Button className="btn btn-danger me-1" onClick={handleOnDelete}>
+          DELETE
+        </Button>
+        <Button className="btn btn-info" onClick={handleOnAdd}>
+          ADD
+        </Button>
+      </div>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Institution Name</th>
+            <th>Website</th>
+            <th>Country</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataToDisplay.map((item, i) => (
+            <tr key={i}>
+              <td>{i + 1}</td>
+              <td>{item.name}</td>
+              <td>
+                <a href={item.web_pages[0]} target="none">
+                  {item.web_pages[0]}
+                </a>
+              </td>
+              <td>{item.country}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 };
 export default DisplayTable;
